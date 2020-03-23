e67f17d4595c67a1aead921f11fd3d28ede19a1c
Dmitry Tarakanov <femto8032@gmail.com>
2020-03-23 19:56:35 +0400

    Initial Commit

---
+++ VideoCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoCollection
+//
+//  Created by Дмитрий Тараканов on 23.03.2020.
+//  Copyright © 2020 Dmitry Angarsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
